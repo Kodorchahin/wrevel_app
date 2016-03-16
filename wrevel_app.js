d17dcb5d905919985e217763804c9e5bf622dd65
Kodor Chahin <kodorchahin@gmail.com>
2016-03-16 16:56:49 +0000

    can login to show room

--- wrevel_app.js
+++ wrevel_app.js
@@ -1,5 +1,36 @@
 if (Meteor.isClient) {
-
+  Template.BTNjoinnow.events({
+    'submit form': function(event, template){
+      event.preventDefault();
+      var emailvar = template.find('#email').value;
+      var unamevar = template.find('#UName').value;
+      var fnamevar = template.find('#FName').value;
+      var lnamevar = template.find('#LName').value;
+      var passwordvar = template.find('#password').value;
+      Accounts.createUser({
+        email: emailvar,
+        password: passwordvar
+      });
+      console.log("Form submitted");
+    }
+  });
+  
+  Template.BTNsignin.events({
+    'submit form': function(event, template){
+      event.preventDefault();
+      var emailvar = template.find('#login-email').value;
+      var passwordvar = template.find('#login-password').value;
+      Meteor.loginWithPassword(emailvar, passwordvar);
+    }
+  });
+  
+  
+  Template.showroom.events({
+    'click.logout': function(event){
+      event.preventDefault();
+      Meteor.logout();
+    }
+  });
 }
 
 
